9fdefa69782d565465df4ceb42798cc5805cfa75
michael <michael@carrolldata.com>
2025-07-16 15:48:21 -0400

    whole bunch of refactoring and cleanup

--- updater.py
+++ updater.py
@@ -4,7 +4,8 @@ import configparser
 import click
 
 class Updater:
-    version = "0.0.6"
+    version = "0.0.7"
+    github_url = "https://api.github.com/repos/${repo}/releases/latest"
 
     def __init__(self, config_dir):
         config = configparser.ConfigParser()
@@ -14,63 +15,64 @@ class Updater:
         
         self.github_repository = config['github']['repository']
         self.github_token = config['github']['token']
+        self.github_headers = {
+            'Accept': "application/vnd.github+json",
+            'Authorization': f"Bearer {self.github_token}",
+            'X-GitHub-Api-Version': "2022-11-28"
+        }
         self.yaml_path = config['yaml']['path']
         self.yaml_dependency = config['yaml']['dependency']
+        self.yaml_lines = []
+        with open(self.yaml_path, 'r+') as fOd:
+            self.yaml_lines = fOd.readlines()
 
     def update(self, isProd=False):
         testToken = '[TEST]' if not isProd else ''
         echo("******** %s DISCORD UPDATER V%s %s ********" % (testToken, self.version, testToken))
         
-        lines, current_dependency = self.find_current_dependency()
-        new_dependency = self.yaml_dependency.replace("${tag}", self.get_latest_tag())
-        if current_dependency != new_dependency:
-            echo("There is an available update!", 1)
-            echo("%s -> %s" % (current_dependency, new_dependency), 2)
+        current_tag = self.get_current_tag()
+        latest_tag = self.get_latest_tag()
+        if current_tag != latest_tag:
+            echo("There is an available update! (%s -> %s)" % (current_tag, latest_tag), 0)
             if isProd:
                 echo("Writing to the yaml file...", 1)
-                self.save_dependency(lines, new_dependency)
+                self.save_yaml(latest_tag)
                 echo("Done!", 1)
         else:
-            echo("No updates at this time.", 1)
+            echo("No updates at this time. (%s latest)" % latest_tag, 0)
 
-    # Grabs the tag name using github's api
+    # Grabs the latest tag name using github's api
     def get_latest_tag(self):
-        headers = {
-            'Accept': "application/vnd.github+json",
-            'Authorization': f"Bearer {self.github_token}",
-            'X-GitHub-Api-Version': "2022-11-28"
-        }
-        result = requests.get(f"https://api.github.com/repos/{self.github_repository}/releases/latest", headers=headers)
+        result = requests.get(self.github_url.replace('${repo}', self.github_repository), headers=self.github_headers)
         tag = result.json().get('tag_name', None)
         if tag is None:
             raise Exception("Could not load the github tag. Do you have a valid github api token in the config?")
-        return result.json()['tag_name']
+        return tag
 
-    # Finds the current tag name by reading the yaml config file
-    def find_current_dependency(self):
-        lines = []
-        with open(self.yaml_path, 'r+') as fOd:
-            lines = fOd.readlines()
-        
-        dependency = ""
-        for line in lines:
-            findDependency = line.find(self.yaml_dependency.replace("${tag}", ""))
+    # Grabs the current tag name by reading the yaml config file
+    def get_current_tag(self):
+        tag = None
+        for line in self.yaml_lines:
+            findDependency = line.find(self._replace_tag())
             if findDependency != -1:
-                dependency = line[findDependency:].replace('\n', '').strip()
+                tag = line[findDependency+len(self._replace_tag()):].replace('\n', '').strip()
                 break
 
-        if dependency == "":
-            raise Exception("Dependency line could not be found in the given yaml file")            
-        return lines, dependency
-    
-    def save_dependency(self, lines, dependency):
+        if tag is None:
+            raise Exception("Current tag could not be found in the given yaml file")            
+        return tag
+
+    def save_yaml(self, tag):
         with open(self.yaml_path, 'w+') as fOd:
-            for line in lines:
-                findDependency = line.find(self.yaml_dependency.replace("${tag}", ""))
+            for line in self.yaml_lines:
+                findDependency = line.find(self._replace_tag())
                 if findDependency != -1:
-                    fOd.write(line[0:findDependency] + dependency + "\n")
+                    fOd.write(line[0:findDependency+len(self._replace_tag())] + tag + "\n")
                 else:
                     fOd.write(line)
+
+    def _replace_tag(self, tag=""):
+        return self.yaml_dependency.replace("${tag}", tag)
 
 
 @click.group()
