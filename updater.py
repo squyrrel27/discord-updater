cb8e4781baeb4fee0002b8e6218b360766df1226
michael <michael@carrolldata.com>
2025-07-17 03:02:07 -0400

    Some small changes to new re

--- updater.py
+++ updater.py
@@ -5,7 +5,7 @@ import configparser
 import click
 
 class Updater:
-    version = "0.0.8"
+    version = "0.0.9"
     github_url = "https://api.github.com/repos/${repo}/releases/latest"
 
     def __init__(self, config_dir):
@@ -21,16 +21,15 @@ class Updater:
             'Authorization': f"Bearer {self.github_token}",
             'X-GitHub-Api-Version': "2022-11-28"
         }
+        newLine = '\\n' if config['yaml']['dependency'].endswith('${tag}') else ''
         self.yaml_path = config['yaml']['path']
-        self.yaml_dependency = config['yaml']['dependency']
-        newLine = '\\n' if self.yaml_dependency.endswith('${tag}') else ''
-        self.yaml_pattern = re.compile(re.escape(self.yaml_dependency).replace("\\$\\{tag\\}", "(.*)" + newLine))
+        self.yaml_pattern = re.compile(re.escape(config['yaml']['dependency']).replace("\\$\\{tag\\}", "(.*)" + newLine))
         with open(self.yaml_path, 'r+') as fOd:
             self.yaml_text = fOd.read()
 
     def update(self, isProd=False):
-        testToken = '[TEST]' if not isProd else ''
-        echo("******** %s DISCORD UPDATER V%s %s ********" % (testToken, self.version, testToken))
+        echo("*********** DISCORD UPDATER v%s ***********" % self.version)
+        echo("Running update..." if isProd else "Running check...")
         
         current_tag = self.get_current_tag()
         latest_tag = self.get_latest_tag()
@@ -49,6 +48,7 @@ class Updater:
         tag = result.json().get('tag_name', None)
         if tag is None:
             raise Exception("Could not load the github tag. Do you have a valid github api token in the config?")
+        
         return tag
 
     # Grabs the current tag name by reading the yaml config file
